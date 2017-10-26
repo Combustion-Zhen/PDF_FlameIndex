correct mixture fraction calculation, default input of phi_f as inf

Counterflow_CH4/counterflow_phi_driver.py
@@ -9,11 +9,11 @@ from counterflow_flame import counterflow_flame
 
 # run cases
 strain = 100.
-phif = [1.3, 1.5, 1.7, 2.0, 2.3, 2.7, 3.2, 2.8, 4.8, 7.0, 9.5, 10.0]
+phif = [1.3, 1.5, 1.7, 2.0, 2.3, 2.7, 3.2, 2.8, 4.8, 7.0, 9.5, float('inf')]
 phio = np.arange(0.1,0.6,0.1)
 
 for phi in phif:
     counterflow_flame(strain_rate=strain,phi_f=phi)
 
-for phi in phif:
+for phi in phio:
     counterflow_flame(strain_rate=strain,phi_o=phi)

Counterflow_CH4/counterflow_phi_plot.py
@@ -42,8 +42,8 @@ for i, var in enumerate(var_names):
 for case in glob.glob('*.xml'):
 
     flame = case[:-4]
-    flame_params = flame.split('_')
-    phi_str = flame_params[3][4:]
+    flame_param = flame.split('_')
+    phi_str = flame_param[3]+flame_param[4]
 
     file_name = '{}.dat'.format(flame)
     data = np.genfromtxt(file_name,skip_header=3,names=True)