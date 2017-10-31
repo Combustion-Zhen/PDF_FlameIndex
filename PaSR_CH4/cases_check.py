191731ffe0883c0e7e65f964e08f1f4a17d09258
Zhen <zhen.lu@kaust.edu.sa>
2017-10-31 16:15:28 +0300

    check cases

--- PaSR_CH4/cases_check.py
+++ PaSR_CH4/cases_check.py
@@ -5,16 +5,16 @@ Check case status
 import subprocess
 import os
 
-date_start = '2017-10-21'
-date_end = '2017-10-22'
-jobid_lb = 4316037
-jobid_ub = 4316518
+date_start = '2017-10-31'
+date_end = '2017-11-01'
+jobid_lb = 4373029
+jobid_ub = 4373509
 
 # output files
 list_fail = open('list_fail.op','w')
 list_comp = open('list_completed.op','w')
 list_time = open('list_timeout.op','w')
-list_step1500 = open('list_step1500.op','w')
+#list_step1500 = open('list_step1500.op','w')
  
 # get job status
 sqout = subprocess.run(['sacct','-S',date_start,'-E',date_end,'-u','luz0a','-b'],
@@ -41,7 +41,7 @@ for line in lines:
         continue
 
     # store the jobid and status
-    if jobid > jobid_lb and jobid < jobid_ub:
+    if jobid >= jobid_lb and jobid <= jobid_ub:
 
         job_status = job[1]
 
@@ -53,6 +53,7 @@ for line in lines:
         file_loc = str(sqout,'utf-8')
         job_name = file_loc[2:file_loc.find(file_name)-1]
 
+        """
         # check steps
         os.chdir(job_name)
         try:
@@ -64,21 +65,25 @@ for line in lines:
         os.chdir('..')
 
         jobs[jobid] = [job_status,job_name,job_steps]
+        """
+        jobs[jobid] = [job_status,job_name]
 
         if job_status == 'COMPLETED':
             num_comp += 1
             list_comp.write('{0}\n'.format(job_name))
         elif job_status == 'FAILED':
             num_fail += 1
-            list_fail.write('{0}\t{1:g}\n'.format(job_name,job_steps))
+            #list_fail.write('{0}\t{1:g}\n'.format(job_name,job_steps))
+            list_fail.write('{0}\n'.format(job_name))
         elif job_status == 'TIMEOUT':
             num_time += 1
-            list_time.write('{0}\t{1:g}\n'.format(job_name,job_steps))
+            #list_time.write('{0}\t{1:g}\n'.format(job_name,job_steps))
+            list_time.write('{0}\n'.format(job_name))
         else:
             num_run += 1
 
-        if job_steps > 1500 :
-            list_step1500.write('{}\n'.format(job_name))
+        #if job_steps > 1500 :
+        #    list_step1500.write('{}\n'.format(job_name))
 
 print('Numbers of FAILED jobs: {:g}'.format(num_fail))
 print('Numbers of COMPLETED jobs: {:g}'.format(num_comp))
@@ -88,4 +93,4 @@ print('Numbers of TIMEOUT jobs: {:g}'.format(num_time))
 list_fail.close()
 list_comp.close()
 list_time.close()
-list_step1500.close()
+#list_step1500.close()
