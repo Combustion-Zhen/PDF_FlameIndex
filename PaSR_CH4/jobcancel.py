82d139a92a6db3292fcea1ac84223d8ddf84fde9
Zhen <zhen.lu@kaust.edu.sa>
2017-11-08 13:41:31 +0300

    jobcancel

--- PaSR_CH4/jobcancel.py
+++ PaSR_CH4/jobcancel.py
@@ -1,5 +1,33 @@
 
-from subprocess import run
+import subprocess
 
-for i in range(4386851,4387001):
-    run(['scancel','{:d}'.format(i)])
+date_start = '2017-11-08'
+date_end = '2017-11-09'
+jobid_lb = 4397304
+jobid_ub = 4397761
+
+# get job status
+sqout = subprocess.run(['sacct',
+                        '-S',date_start,
+                        '-E',date_end,
+                        '-u','luz0a',
+                        '-b'],
+                       check=True,
+                       stdout=subprocess.PIPE
+                       ).stdout
+
+lines = str(sqout,'utf-8').split('\n')
+
+for line in lines:
+
+    job = line.split()
+    # skip empty lines
+    if not job: continue
+    try:
+        jobid = int(job[0])
+    except ValueError:
+        continue
+
+    # cancel job
+    if jobid >= jobid_lb and jobid <= jobid_ub:
+        subprocess.run(['scancel','{:d}'.format(jobid)])
