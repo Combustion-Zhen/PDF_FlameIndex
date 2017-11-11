747893c8a18117319abcfe4762c4595a52d4b95b
Zhen <zhen.lu@kaust.edu.sa>
2017-11-11 19:44:11 +0300

    resubmit HYB cases

--- PaSR_CH4/cases_continue.py
+++ PaSR_CH4/cases_continue.py
@@ -9,115 +9,164 @@ import shutil
 from subprocess import run
 from counterflow_file import params2name
 
+mixing_models = {'IEMHYB':4,
+                 'MCHYB':5,
+                 'EMSTHYB':6,
+                 'IEM':7,
+                 'MC':8,
+                 'EMST':9}
+time_res = [1.e-2,]
+mix_res_ratio = [0.02, 0.05, 0.1, 0.2, 0.5]
+equiv_ratio_f = [4.76,]
+equiv_ratio = [1.0, 1.2, 1.4]
+Zf_variance = [0.01, 0.02, 0.05, 0.1, 0.15]
+dtmix = [0.01,]
+
+params = {}
+
+for mix_k, mix_v in mixing_models.items():
+    params['MIX'] = mix_k
+    for tres in time_res:
+        params['tres'] = tres
+        for tmix_ratio in mix_res_ratio:
+            params['tmix'] = tmix_ratio
+            for eqv in equiv_ratio:
+                params['eqv'] = eqv
+                for var in Zf_variance:
+                    params['Zfvar'] = var
+                    for dt in dtmix:
+                        params['dtmix'] = dt
+                        for phi in equiv_ratio_f:
+                            params['phif']=phi
+
+                            case = params2name(params)
+
+                            if os.path.isdir(case):
+                                print(case)
+                                os.chdir(case)
+                                
+                                with open('pasr.nml','r') as f:
+                                    lines = f.readlines()
+
+                                with open('pasr.nml','w') as f:
+                                    for line in lines:
+                                        if line.startswith('part_in'):
+                                            line = 'part_in=.true.\n'
+                                        f.write(line)
+
+                                run(['sbatch','run_shaheen.sh'])
+
+                                os.chdir('..')
+
 #mixing_models = {'IEM':7,'MC':8,'EMST':9}
 #time_res = [4.e-3, 1.e-2]
 #mix_res_ratio = [0.02, 0.05, 0.1, 0.2, 0.5]
 #equiv_ratio = [1.0, 1.2]
 #Zf_variance = [0.01, 0.02, 0.05, 0.1]
 #dtmix = [0.01, 0.04]
-
-mixing_models = {'IEM':7,'MC':8,'EMST':9}
-time_res = [1.e-2,]
-mix_res_ratio = [0.02, 0.05, 0.1, 0.2, 0.5]
-equiv_ratio = [1.0, 1.2, 1.4]
-Zf_variance = [0.02, 0.05, 0.1]
-dtmix = [0.01,]
-phif=[4.76,]
-
-params = {}
-
-for mix_k, mix_v in mixing_models.items():
-    params['MIX'] = mix_k
-    for tres in time_res:
-        params['tres'] = tres
-        for tmix_ratio in mix_res_ratio:
-            params['tmix'] = tmix_ratio
-            for eqv in equiv_ratio:
-                params['eqv'] = eqv
-                for var in Zf_variance:
-                    params['Zfvar'] = var
-                    for dt in dtmix:
-                        params['dtmix'] = dt
-                        for phi in phif:
-                            params['phif']=phi
-                        
-
-                            case = params2name(params)
-
-                            #params_new = copy.deepcopy(params)
-                            #params_new['MIX'] = '{}HYB'.format(params['MIX'])
-                            #case_new = params2name(params_new)
-
-                            if os.path.isdir(case):
-                                ##copy for hybrid model
-                                #shutil.copytree(case,case_new)
-                                print(case)
-                                os.chdir(case)
-                                
-                                with open('pasr.nml','r') as f:
-                                    lines = f.readlines()
-
-                                with open('pasr.nml','w') as f:
-                                    for line in lines:
-                                        if line[:7]=='part_in':
-                                            line = 'part_in=.true.\n'
-                                        if line[:6]=='mxmode':
-                                            line = 'mxmode={:d}\n'.format(mix_v)
-                                        f.write(line)
-
-                                run(['sbatch','run_shaheen.sh'])
-
-                                os.chdir('..')
-
-Zf_variance = [0.02, 0.04]
-phif=[2,]
-
-params = {}
-
-for mix_k, mix_v in mixing_models.items():
-    params['MIX'] = mix_k
-    for tres in time_res:
-        params['tres'] = tres
-        for tmix_ratio in mix_res_ratio:
-            params['tmix'] = tmix_ratio
-            for eqv in equiv_ratio:
-                params['eqv'] = eqv
-                for var in Zf_variance:
-                    params['Zfvar'] = var
-                    for dt in dtmix:
-                        params['dtmix'] = dt
-                        for phi in phif:
-                            params['phif']=phi
-                        
-
-                            case = params2name(params)
-
-                            #params_new = copy.deepcopy(params)
-                            #params_new['MIX'] = '{}HYB'.format(params['MIX'])
-                            #case_new = params2name(params_new)
-
-                            if os.path.isdir(case):
-                                ##copy for hybrid model
-                                #shutil.copytree(case,case_new)
-                                print(case)
-                                os.chdir(case)
-                                
-                                with open('pasr.nml','r') as f:
-                                    lines = f.readlines()
-
-                                with open('pasr.nml','w') as f:
-                                    for line in lines:
-                                        if line[:7]=='part_in':
-                                            line = 'part_in=.true.\n'
-                                        if line[:6]=='mxmode':
-                                            line = 'mxmode={:d}\n'.format(mix_v)
-                                        f.write(line)
-
-                                run(['sbatch','run_shaheen.sh'])
-
-                                os.chdir('..')
-
-
+#
+#mixing_models = {'IEM':7,'MC':8,'EMST':9}
+#time_res = [1.e-2,]
+#mix_res_ratio = [0.02, 0.05, 0.1, 0.2, 0.5]
+#equiv_ratio = [1.0, 1.2, 1.4]
+#Zf_variance = [0.02, 0.05, 0.1]
+#dtmix = [0.01,]
+#phif=[4.76,]
+#
+#params = {}
+#
+#for mix_k, mix_v in mixing_models.items():
+#    params['MIX'] = mix_k
+#    for tres in time_res:
+#        params['tres'] = tres
+#        for tmix_ratio in mix_res_ratio:
+#            params['tmix'] = tmix_ratio
+#            for eqv in equiv_ratio:
+#                params['eqv'] = eqv
+#                for var in Zf_variance:
+#                    params['Zfvar'] = var
+#                    for dt in dtmix:
+#                        params['dtmix'] = dt
+#                        for phi in phif:
+#                            params['phif']=phi
+#                        
+#
+#                            case = params2name(params)
+#
+#                            #params_new = copy.deepcopy(params)
+#                            #params_new['MIX'] = '{}HYB'.format(params['MIX'])
+#                            #case_new = params2name(params_new)
+#
+#                            if os.path.isdir(case):
+#                                ##copy for hybrid model
+#                                #shutil.copytree(case,case_new)
+#                                print(case)
+#                                os.chdir(case)
+#                                
+#                                with open('pasr.nml','r') as f:
+#                                    lines = f.readlines()
+#
+#                                with open('pasr.nml','w') as f:
+#                                    for line in lines:
+#                                        if line[:7]=='part_in':
+#                                            line = 'part_in=.true.\n'
+#                                        if line[:6]=='mxmode':
+#                                            line = 'mxmode={:d}\n'.format(mix_v)
+#                                        f.write(line)
+#
+#                                run(['sbatch','run_shaheen.sh'])
+#
+#                                os.chdir('..')
+#
+#Zf_variance = [0.02, 0.04]
+#phif=[2,]
+#
+#params = {}
+#
+#for mix_k, mix_v in mixing_models.items():
+#    params['MIX'] = mix_k
+#    for tres in time_res:
+#        params['tres'] = tres
+#        for tmix_ratio in mix_res_ratio:
+#            params['tmix'] = tmix_ratio
+#            for eqv in equiv_ratio:
+#                params['eqv'] = eqv
+#                for var in Zf_variance:
+#                    params['Zfvar'] = var
+#                    for dt in dtmix:
+#                        params['dtmix'] = dt
+#                        for phi in phif:
+#                            params['phif']=phi
+#                        
+#
+#                            case = params2name(params)
+#
+#                            #params_new = copy.deepcopy(params)
+#                            #params_new['MIX'] = '{}HYB'.format(params['MIX'])
+#                            #case_new = params2name(params_new)
+#
+#                            if os.path.isdir(case):
+#                                ##copy for hybrid model
+#                                #shutil.copytree(case,case_new)
+#                                print(case)
+#                                os.chdir(case)
+#                                
+#                                with open('pasr.nml','r') as f:
+#                                    lines = f.readlines()
+#
+#                                with open('pasr.nml','w') as f:
+#                                    for line in lines:
+#                                        if line[:7]=='part_in':
+#                                            line = 'part_in=.true.\n'
+#                                        if line[:6]=='mxmode':
+#                                            line = 'mxmode={:d}\n'.format(mix_v)
+#                                        f.write(line)
+#
+#                                run(['sbatch','run_shaheen.sh'])
+#
+#                                os.chdir('..')
+#
+#
 #time_res = [4.e-3,]
 #Zf_variance = [0.02, 0.05]
 #
