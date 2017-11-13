a7bf39e511a203fa0327d10d6cc333f80d928e94
Zhen <zhen.lu@kaust.edu.sa>
2017-11-13 21:54:11 +0300

    conditional omega

--- PaSR_CH4/plot_omega.py
+++ PaSR_CH4/plot_omega.py
@@ -3,7 +3,8 @@ Zhen Lu 2017/11/13
 
 plot conditional omega
 """
-
+import os
+import copy
 import numpy as np
 import matplotlib as mpl
 mpl.use('Agg')
@@ -16,28 +17,36 @@ tres = 0.01
 mix_res_ratio = [0.02, 0.05, 0.1, 0.2, 0.5]
 phif = 4.76
 phi = [1.0, 1.2, 1.4]
-Zf_variance = [0.01, 0.02, 0.05, 0.1, 0.15]
+#Zf_variance = [0.01, 0.02, 0.05, 0.1, 0.15]
+Zf_variance = [0.05, 0.1, 0.15]
+line_style = ['-.','-','--']
 dtmix = 0.01
 
 params = {}
-params['MIX'] = None
-params['tres'] = None
+params['MIX'] = model
+params['tres'] = tres
 params['tmix'] = None
 params['eqv'] = None
 params['Zfvar'] = None
-params['dtmix'] = None
-params['phif'] = None
+params['dtmix'] = dtmix
+params['phif'] = phif
 
-dst = 'figs_FI_ave'
-dat_name = 'pasrm.op'
+plot_params = {}
+plot_params['tmix'] = None
+plot_params['eqv'] = None
+#plot_params['Zfvar'] = None
+
+dst = 'figs_omega_all'
+dat_name = 'mixrate.op'
+ave_name = 'pasrm.op'
 
 # plot
 # figure and axes parameters
 # total width is fixed, for one column plot
 plot_width    = 6.7
-margin_left   = 1.0
-margin_right  = 0.25
-margin_bottom = 0.85
+margin_left   = 1.2
+margin_right  = 0.1
+margin_bottom = 0.9
 margin_top    = 0.1
 space_width   = 0.
 space_height  = 0.
@@ -66,4 +75,86 @@ plot_height = (num_rows*subplot_height
               +margin_top
               +(num_rows-1)*space_height)
 
+Zall = np.genfromtxt('flame_Z.dat',skip_header=1,usecols=(0,))
+Z = (Zall[1:]+Zall[:-1])/2
 
+for eqv in phi:
+    params['eqv'] = eqv
+    plot_params['eqv'] = eqv
+    for tmixr in mix_res_ratio:
+        params['tmix'] = tmixr
+        plot_params['tmix'] = tmixr
+
+        fig, ax = plt.subplots(
+                figsize = cm2inch(plot_width,plot_height))
+
+        omtau = 1/(tres*tmixr)
+        ax.plot([0,0.2],[omtau,omtau],'k:',linewidth=1)
+
+        for i, var in enumerate(Zf_variance):
+            params['Zfvar'] = var
+
+            case = params2name(params)
+            file_name = '/'.join([model,case,dat_name])
+
+            rate = np.genfromtxt(file_name)
+
+            file_name = '/'.join([model,case,ave_name])
+            omt = np.genfromtxt(file_name,usecols=(4,))
+
+            if len(rate.shape) == 1:
+                continue
+
+            # calculate the ensemble averate
+            omega = np.mean(rate[-50:,1:-1],axis=0)
+            omstd = np.std(rate[-50:,1:-1],axis=0)
+            omave = np.mean(omt[-50:])
+
+            label = ''.join(
+                    [r'$\eta_{Z,r}=$',
+                     '{:g}'.format(var),
+                     '\n',
+                     r'$\langle\omega\rangle\;\,=$',
+                     '{:g}'.format(omave),
+                     r'$\;\mathrm{s}^{-1}$'
+                     ])
+
+            ax.plot(Z,omega,line_style[i],linewidth=1,label=label)
+            #ax.fill_between(Z,omega+omstd,omega-omstd,alpha=0.1)
+
+#        ax.text(
+#                0.25,0.14,
+#                ''.join([
+#                    r'$\tau_{\mathrm{res}}\,=0.01\;\mathrm{s}$',
+#                    '\n',
+#                    r'$\tau_{\mathrm{mix}}\!=2\;\mathrm{ms}$',
+#                    '\n',
+#                    r'$\varphi\quad\;\!=1.2$',
+#                    '\n',
+#                    r'$\langle\varphi_r\rangle\!=4.76$',
+#                    '\n',
+#                    r'$\eta_{Z,r}\!\!\:=0.1$'
+#                    ]))
+
+        ax.legend(
+                frameon=False,
+                labelspacing=1
+                )
+        ax.set_xlim(0.02,0.13)
+        ax.set_xlabel(r'$Z$')
+        ax.set_ylabel(r'$\langle\widetilde{\omega\vert Z}\rangle$')
+        ax.set_yscale('log')
+        
+        fig.subplots_adjust(
+                left = margin_left/plot_width,
+                bottom = margin_bottom/plot_height,
+                right = 1.0-margin_right/plot_width,
+                top = 1.0-margin_top/plot_height,
+                wspace = space_width/plot_width,
+                hspace = space_height/plot_height
+                )
+
+        plot_name = params2name(plot_params)
+        fig.savefig('{}/{}.pdf'.format(dst,plot_name))
+
+        plt.close('all')
